955edba063b86828d594fd539e957bf2b5dc6182
PriyanshuSaini02 <priyanshu688.be22@chitkara.edu.in>
2024-10-22 15:40:56 +0530

    Commit

--- server/server.js
+++ server/server.js
@@ -3,16 +3,20 @@ const connectDb=require("./config/dbConnection");
 const errorHandler=require("./middleware/errorHandler");
 const cors=require("cors");
 
+const dotenv=require("dotenv");
+dotenv.config();
+
 connectDb();
 const app=express();
 const port=process.env.port || 5000;
 
-app.use(express.js);
+app.use(express.json);
+app.use(cors());
+app.use(errorHandler);
 
 app.get('/',(req,res)=>{
     res.send("working");
 });
-app.use(cors());
 
 app.listen(port,()=>{
     console.log(`server running on port http://localhost:${port}`);
