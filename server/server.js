4154237e5eed2c110e7fa5c17502483a17d80684
PriyanshuSaini02 <priyanshu688.be22@chitkara.edu.in>
2024-10-23 15:23:08 +0530

    Commit

--- server/server.js
+++ server/server.js
@@ -1,23 +1,38 @@
-const express=require("express");
-const connectDb=require("./config/dbConnection");
-const errorHandler=require("./middleware/errorHandler");
-const cors=require("cors");
+const express = require("express");
+const connectDb = require("./config/dbConnection");
+//error handling middleware
+const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
+const path = require("path");
 
-const dotenv=require("dotenv");
+const dotenv = require("dotenv");
 dotenv.config();
 
 connectDb();
-const app=express();
-const port=process.env.port || 5000;
+const app = express();
+const port = process.env.port || 5000;
 
+app.set('view engine', 'hbs');
 app.use(express.json);
 app.use(cors());
 app.use(errorHandler);
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("working");
 });
 
-app.listen(port,()=>{
+app.get("/home", (req, res) => {
+    res.render("home")
+})
+
+app.get("alluser", (req, res) => {
+    res.render("users", {
+        users: [{
+            id: 1, username: "nitesh",age: 23},
+            { id: 1, username: "akash", age: 24 }]
+    })
+})
+
+app.listen(port, () => {
     console.log(`server running on port http://localhost:${port}`);
 });
